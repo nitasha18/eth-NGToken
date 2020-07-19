b3361bb7f4b8d358a9b223577a915c243980eaf7
nitasha18 <nitashag18@gmail.com>
2020-07-20 00:17:47 +0530

    updated with transfer and approved tokens

--- contracts/NGToken.sol
+++ contracts/NGToken.sol
@@ -8,12 +8,29 @@ contract NGToken {
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-    //function transfer(address _to, uint256 _value) public returns (bool success) {
-         
-    //}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    //delegated transfer
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+
 }
--- test/NGToken.js
+++ test/NGToken.js
@@ -28,4 +28,49 @@ contract('NGToken',function(accounts){
             assert.equal(adminBalance.toNumber(),1000000,'it allocates the initial supply to the admin account');
         });
     });
+
+    it('transfers token ownership', function() {
+        return NGToken.deployed().then(function(instance) {
+          tokenInstance = instance;
+          return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+        }).then(assert.fail).catch(function(error) {
+          assert(error.message, 'error message must contain revert');
+          return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(success) {
+          assert.equal(success, true, 'it returns true');
+          return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(receipt) {
+          assert.equal(receipt.logs.length, 1, 'triggers one event');
+          assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+          assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+          assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+          assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+          return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+          return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+        });
+      });
+
+    it('approves tokens for delegated transfers',function(){
+        return NGToken.deployed().then(function(instance){
+            tokenInstance=instance;
+            return tokenInstance.approve.call(accounts[1],100);
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.approve(accounts[1],100,{ from: accounts[0] });
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized from');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber() ,100,'stores the allowance for delegated transfer')
+        });
+    });
+
 })
